3931f0857ceec69c7c2bcbc5a3bceb2f2608fa37
noel <novocorboudnoel@gmail.com>
2026-06-06 01:37:58 +0200

    back to requirements xD

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -70,11 +70,11 @@ resource "null_resource" "setup_api_in_container" {
       <<-EOF
       set -ex
       pct exec ${proxmox_lxc.api.vmid} -- apk update
-      pct exec ${proxmox_lxc.api.vmid} -- apk add --no-cache git python3 mariadb-dev gcc musl-dev python3-dev build-base linux-headers uv
+      pct exec ${proxmox_lxc.api.vmid} -- apk add --no-cache git python3 mariadb-dev gcc musl-dev python3-dev build-base linux-headers
 
       pct exec ${proxmox_lxc.api.vmid} -- git clone https://github.com/noelpatata/WalletTrackerAPI.git /srv/WalletTrackerAPI
 
-      pct exec ${proxmox_lxc.api.vmid} -- uv sync --no-dev --frozen --directory /srv/WalletTrackerAPI/app
+      pct exec ${proxmox_lxc.api.vmid} -- pip install --no-cache-dir -r /srv/WalletTrackerAPI/app/requirements.txt
 
       pct exec ${proxmox_lxc.api.vmid} -- apk del gcc musl-dev build-base linux-headers
 
@@ -139,9 +139,9 @@ resource "null_resource" "deploy_api" {
       set -ex
       pct exec ${proxmox_lxc.api.vmid} -- git -C /srv/WalletTrackerAPI pull
 
-      pct exec ${proxmox_lxc.api.vmid} -- uv sync --no-dev --frozen --directory /srv/WalletTrackerAPI/app
+      pct exec ${proxmox_lxc.api.vmid} -- pip install --no-cache-dir -r /srv/WalletTrackerAPI/app/requirements.txt
 
-      pct exec ${proxmox_lxc.api.vmid} -- sh -c "export DATABASE_ROOT_PASSWORD='${data.vault_kv_secret_v2.backend.data["MARIADB_ROOT_PASSWORD"]}'; export DATABASE_NAME='wallet_tracker'; export WALLET_TRACKER_DB_USER='root'; export WALLET_TRACKER_DB_HOST='${var.db_container_ip}'; /srv/WalletTrackerAPI/app/.venv/bin/python /srv/WalletTrackerAPI/app/migrate_all.py"
+      pct exec ${proxmox_lxc.api.vmid} -- sh -c "export DATABASE_ROOT_PASSWORD='${data.vault_kv_secret_v2.backend.data["MARIADB_ROOT_PASSWORD"]}'; export DATABASE_NAME='wallet_tracker'; export WALLET_TRACKER_DB_USER='root'; export WALLET_TRACKER_DB_HOST='${var.db_container_ip}'; python /srv/WalletTrackerAPI/app/migrate_all.py"
 
       pct push ${proxmox_lxc.api.vmid} /tmp/wallettracker.init /etc/init.d/wallettracker
       rm /tmp/wallettracker.init
